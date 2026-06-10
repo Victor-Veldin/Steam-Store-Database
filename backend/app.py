c53e623e2d79893ab1f713d1d29c383c59ee6a88
Victor Raffucci <victor.raffucci2@upr.edu>
2026-06-10 15:26:41 -0400

    Added game details page with genres developers publishers and platforms

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__,
@@ -9,6 +9,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'steam_project'
+app.config['MYSQL_PORT'] = 3306
 
 mysql = MySQL(app)
 
@@ -21,23 +22,32 @@ def home():
 
 
 # ==========================
-# GAMES
+# GAMES PAGE + SEARCH
 # ==========================
 @app.route("/games")
 def games():
+    search = request.args.get("search", "")
+    print("SEARCH VALUE:", search)
     cur = mysql.connection.cursor()
 
-    cur.execute("""
-        SELECT appid, name, release_date, price
-        FROM Game
-        LIMIT 100
-    """)
+    if search:
+        cur.execute("""
+            SELECT appid, name, release_date, price
+            FROM Game
+            WHERE name LIKE %s
+            LIMIT 100
+        """, ('%' + search + '%',))
+    else:
+        cur.execute("""
+            SELECT appid, name, release_date, price
+            FROM Game
+            LIMIT 100
+        """)
 
     games = cur.fetchall()
     cur.close()
 
-    return render_template("games.html", games=games)
-
+    return render_template("games.html", games=games, search=search)
 
 # ==========================
 # GENRES
@@ -77,7 +87,67 @@ def developers():
 
     return render_template("developers.html", developers=developers)
 
+# ==========================
+# GAME DETAILS PAGE
+# ==========================
 
+@app.route("/game/<int:appid>")
+def game_details(appid):
+    cur = mysql.connection.cursor()
+
+    cur.execute("""
+        SELECT appid, name, release_date, english, required_age,
+               achievements, positive_ratings, negative_ratings,
+               average_playtime, median_playtime, owners, price
+        FROM Game
+        WHERE appid = %s
+    """, (appid,))
+    game = cur.fetchone()
+
+    cur.execute("""
+        SELECT ge.genre_name
+        FROM Genre ge
+        JOIN GameGenre gg ON ge.genre_id = gg.genre_id
+        WHERE gg.appid = %s
+    """, (appid,))
+    genres = cur.fetchall()
+
+    cur.execute("""
+        SELECT d.developer_name
+        FROM Developer d
+        JOIN GameDeveloper gd ON d.developer_id = gd.developer_id
+        WHERE gd.appid = %s
+    """, (appid,))
+    developers = cur.fetchall()
+
+    cur.execute("""
+        SELECT p.publisher_name
+        FROM Publisher p
+        JOIN GamePublisher gp ON p.publisher_id = gp.publisher_id
+        WHERE gp.appid = %s
+    """, (appid,))
+    publishers = cur.fetchall()
+
+    cur.execute("""
+        SELECT p.platform_name
+        FROM Platform p
+        JOIN GamePlatform gp
+        ON p.platform_id = gp.platform_id
+        WHERE gp.appid = %s
+    """, (appid,))
+
+    platforms = cur.fetchall()
+    
+    cur.close()
+
+    return render_template(
+        "game_details.html",
+        game=game,
+        genres=genres,
+        developers=developers,
+        publishers=publishers,
+        platforms=platforms
+    )
 # ==========================
 # RUN APP
 # ==========================
